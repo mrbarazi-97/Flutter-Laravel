aa978712baf048c81ef8398bff5e94680291b36f
mrbarazi-97 <you@example.com>
2021-07-04 09:31:19 -0700

    craete MaterialApp and Scafold widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,41 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'home.dart';
+//import 'home.dart';
 
 void main() {
-  runApp(Home());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(primarySwatch: Colors.blue),
+    home: Scaffold(
+        appBar: AppBar(
+          title: Text("App title"),
+          actions: <Widget>[Icon(Icons.search), Icon(Icons.shopping_cart)],
+        ),
+        drawer: Drawer(),
+        //body: Home(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.purple,
+                ),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.purpleAccent,
+                  la
+                ),
+                
+                label: 'Account'),
+          ],
+          selectedLabelStyle: TextStyle(fontSize: 22, color: Colors.red),
+          selectedItemColor: Colors.green,
+        )),
+  ));
 }
