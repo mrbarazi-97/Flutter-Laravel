431900aa6048b3e3af59cfa3aee44e7767ce209a
mrbarazi-97 <you@example.com>
2021-07-04 07:44:16 -0700

    create widget

---
+++ lib/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Text(
+        "Ya Allah",
+        textDirection: TextDirection.ltr,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(Home());
+}
